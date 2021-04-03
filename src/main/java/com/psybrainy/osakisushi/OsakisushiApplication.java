a4bab2f25446999d5748fda8fb0e8a91bba15ec9
PsyBrainy <martuto93@gmail.com>
2021-04-03 17:48:38 -0300

    init

---
+++ src/main/java/com/psybrainy/osakisushi/OsakisushiApplication.java
@@ -0,0 +1,13 @@
+package com.psybrainy.osakisushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsakisushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsakisushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybrainy/osakisushi/OsakisushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybrainy.osakisushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsakisushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
